175e7cb1193c1d95bbd437f2c711a0ef3dc21e44
avi <aviavissar@gmail.com>
2018-11-11 03:19:32 +0200

    put carncy

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
+
+  // load a locale
+  numeral.register('locale', 'il', {
+    delimiters: {
+        thousands: ' ',
+        decimal: '.'
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: ' ש"ח '
+    }
+  });
+  
+  numeral.locale('il');
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+
+
+
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>{numeral(amount/100).format('$0,0.00')}
+    &nbsp;&nbsp;&nbsp;&nbsp; 
+     {moment(createdAt).format(' DD/MM/YYYY')}</p>
   </div>
 );
 
