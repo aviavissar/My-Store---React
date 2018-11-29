8b0fc6e8e629b022a784fd55467aa2e0d34b37eb
avi <aviavissar@gmail.com>
2018-11-29 22:16:44 +0200

    Remove yarn lock file

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,26 +3,28 @@ import { connect } from 'react-redux';
 import getExpensesTotal from '../selectors/expenses-total.js';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
+import {Link} from 'react-router-dom'
 
 
 
 
 
 const ExpensesSummary = (props) => (
-    <div>
-        <h3>
-            {
-                "the number of items is "+props.expenses.length+" the total amount is "+
-              numeral((getExpensesTotal(props.expenses))/100).format('$0,0.00')
-
-            }
-        </h3>
-
+    <div className="page-header">
+        <div className="content-container">
+            <h1 className="page-header__title">
+                Viewing <span>{props.expenses.length}</span> expenses totalling <span>
+                   {numeral((getExpensesTotal(props.expenses)) / 100).format('$0,0.00')}</span>
+            </h1>
+            <div className="page-header__action">
+                <Link className="button" to="/create">Add Expense</Link>
+            </div>
+        </div>
     </div>
 );
 const mapStateToProps = (state) => {
     return {
-      expenses: selectExpenses(state.expenses, state.filters)
+        expenses: selectExpenses(state.expenses, state.filters)
     };
-  };
+};
 export default connect(mapStateToProps)(ExpensesSummary);
--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {startLogin} from '../actions/auth'
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth'
 
-export const Login = ({startLogin}) => (
-    <div>
-        <h1>Login Page</h1>
-      
-                <div><button onClick={startLogin} >login</button></div>
+export const Login = ({ startLogin }) => (
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className='box-layout__title'>Expensify App</h1>
+            <p>It's time to get your expenses under control.</p>
+            <button className="button" onClick={startLogin} >login with Google</button></div>
     </div>
 );
 
-const mapDispatchToProps=(dispatch)=>({
-    startLogin:()=>dispatch(startLogin())
+const mapDispatchToProps = (dispatch) => ({
+    startLogin: () => dispatch(startLogin())
 })
 
-export default connect(undefined,mapDispatchToProps)(Login) ;
+export default connect(undefined, mapDispatchToProps)(Login);
